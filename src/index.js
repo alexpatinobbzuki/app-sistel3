35c49966c8b2762c8351bf744e9f8526d654250f
Valeria <valeria@example.com>
2024-08-26 11:50:25 -0500

    Creamos base del proyecto

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+//configuraciones
+const app = express()
+const port = 3000
+
+//rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
